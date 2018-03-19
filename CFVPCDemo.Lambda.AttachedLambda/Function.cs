cc89d8dfdad10fb3e8781ef9bd0a200015bc1573
Justin Baacke <jbaacke@kiosk.com>
2018-03-19 05:42:41 -0600

    Add Example attached Lambda

---
+++ CFVPCDemo.Lambda.AttachedLambda/Function.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace CFVPCDemo.Lambda.AttachedLambda
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(object input, ILambdaContext context)
+        {
+            context.Logger.LogLine("Hello DevOps Meetup");
+
+            return "Sucess!";
+        }
+    }
+}
